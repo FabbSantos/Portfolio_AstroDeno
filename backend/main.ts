7a0be41e14da3a4383fa9d93f9cd40db4c3cf3a6
Fabrício Bahiense Santos <fabriciobs2000@gmail.com>
2024-07-18 00:32:16 -0300

    project divided in two, beggining of api
    Co-authored-by: Bruno Oliveira Julião <brunojuliao@users.noreply.github.com>

---
+++ backend/main.ts
@@ -0,0 +1,26 @@
+// oak is pretty much similar to express in nodejs
+import { Application } from "jsr:@oak/oak/application";
+import { Router } from "jsr:@oak/oak/router";
+import works from "./models/work.json" with { type: "json" };
+
+const router = new Router();
+
+router
+  .get("/works", (context) => {
+    console.log('Processando requisição! /works');
+    context.response.body = works;
+  })
+  .get("/", (context) => {
+    console.log('Processando requisição! /');
+    context.response.body = "Hello world!";
+  });
+
+// Create Application Like Express
+const app = new Application();
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+const opt = { hostname: "localhost", port: 5000 };
+await app.listen(opt);
+console.log(`Starting ${opt}`);
