bbd4dd5d20620a27070a8934bfc5e2cd157b52f9
Bruno Juliao <juliaob@curtisinst.com>
2024-07-21 12:26:46 -0300

    Adding devcontainer just in case... Everything worked fine in it. Replaced Vercel by Deno adapter.

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+// import vercel from "@astrojs/vercel/serverless";
+import deno from "@deno/astro-adapter";
 
 import vue from "@astrojs/vue";
 
@@ -8,7 +9,8 @@ import vue from "@astrojs/vue";
 export default defineConfig({
   integrations: [tailwind(), vue()],
   output: "server",
-  adapter: vercel({
+  //adapter: vercel({
+  adapter: deno({
     webAnalytics: {
       enabled: true
     }
